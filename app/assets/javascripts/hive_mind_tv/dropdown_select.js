a1833b1503a02e25f298efcc2044ac8defc645e5
Joe Haig <joe.haig@bbc.co.uk>
2016-02-25 15:19:32 +0000

    Remove hardwired values

--- app/assets/javascripts/hive_mind_tv/dropdown_select.js
+++ app/assets/javascripts/hive_mind_tv/dropdown_select.js
@@ -1,12 +1,17 @@
 $(function(){
   $(".dropdown-menu li a").click(function(event) {
+    var hive = $(".dropdown-menu .hive_id");
+    var device_id = $(".dropdown-menu .device_id").val();
+    var hive_id = null;
     event.preventDefault();
     if (event.toElement.id === 'disconnect') {
       url = '/api/plugin/hive/disconnect';
-      hive_id = 6;
+      hive_id = hive.val();
+      var new_hive_id = null;
     } else {
       url = '/api/plugin/hive/connect';
       hive_id = event.toElement.id
+      var new_hive_id = hive_id;
     }
     var label = $(".btn:first-child");
     var text = $(this).text();
@@ -16,11 +21,12 @@ $(function(){
       data: {
         'connection': {
           'hive_id': hive_id,
-          'device_id': 1
+          'device_id': device_id
         },
         success: function(data){
           label.text(text);
           label.val(text);
+          hive.val(new_hive_id);
         },
       }
     }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
